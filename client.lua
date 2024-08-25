de08b1c41c68b98f696857db1acd851b2d728403
xinarox <106871624+xinarox@users.noreply.github.com>
2024-08-26 00:28:53 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,9 +1,7 @@
--- Tableau pour stocker les coordonnées des herse
 local hersePositions = {}
 
--- Cette fonction récupère les positions des herse à partir des entités du monde
 function GetHersePositions()
-    local objects = GetGamePool("CObject")  -- Récupère toutes les entités objets du jeu
+    local objects = GetGamePool("CObject") 
     for _, object in ipairs(objects) do
         local modelHash = GetEntityModel(object)
         if modelHash == GetHashKey("p_ld_stinger_s") then
@@ -13,23 +11,17 @@ function GetHersePositions()
     end
 end
 
--- Fonction pour détecter les collisions avec les herse
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-
-        -- Mettre à jour les positions des herse à chaque boucle
-        hersePositions = {}  -- Réinitialiser la liste des positions des herse
-        GetHersePositions()   -- Mettre à jour les positions des herse
-
-        -- Vérifie si un véhicule entre en collision avec une des herse
+        hersePositions = {}  
+        GetHersePositions()   
         local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
         if DoesEntityExist(vehicle) then
             local vehicleCoords = GetEntityCoords(vehicle)
             for _, herseCoords in ipairs(hersePositions) do
                 local distance = GetDistanceBetweenCoords(vehicleCoords, herseCoords, true)
-                if distance < 5.0 then  -- Ajustez la distance de détection si nécessaire
-                    -- Vérifie si au moins un pneu du véhicule n'est pas déjà crevé
+                if distance < 5.0 then 
                     local allTiresIntact = true
                     for i = 0, GetVehicleNumberOfWheels(vehicle) - 1 do
                         if IsVehicleTyreBurst(vehicle, i, false) then
@@ -37,8 +29,6 @@ Citizen.CreateThread(function()
                             break
                         end
                     end
-
-                    -- Si tous les pneus sont intacts, crever tous les pneus du véhicule
                     if allTiresIntact then
                         for i = 0, GetVehicleNumberOfWheels(vehicle) - 1 do
                             SetVehicleTyreBurst(vehicle, i, true, 1000.0)
